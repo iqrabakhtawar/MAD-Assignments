13fd78003267225e30895b87fb7c94c37f3350fe
iqrabakhtawar <103189596+iqrabakhtawar@users.noreply.github.com>
2022-12-18 23:29:43 +0500

    Add files via upload

--- Theory Assignment/App.js
+++ Theory Assignment/App.js
@@ -1,260 +1,107 @@
-import { StyleSheet, View } from "react-native";
-export default function Ludo() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.wrapper}>
-        <View style={styles.Box1}>
-          <View style={styles.box1}>
-            <View style={styles.row}>
-              <View style={styles.circle}></View>
-              <View style={styles.circle}></View>
-            </View>
-            <View style={styles.row}>
-              <View style={styles.circle}></View>
-              <View style={styles.circle}></View>
-            </View>
-          </View>
-        </View>
-        <View style={styles.wrapper2}>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-        </View>
-        <View style={styles.wrapper2}>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-        </View>
-        <View style={styles.wrapper2}>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-        </View>
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import {View,Text,Button,Pressable,TextInput,Image} from 'react-native';
 
-        <View style={styles.Box2}>
-          <View style={styles.box2}>
-            <View style={styles.row}>
-              <View style={styles.circle}></View>
-              <View style={styles.circle}></View>
-            </View>
-            <View style={styles.row}>
-              <View style={styles.circle}></View>
-              <View style={styles.circle}></View>
-            </View>
-          </View>
-        </View>
+
+const Stack=createNativeStackNavigator();
+const App =() =>
+{
+  return(
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} options={{ headerStyle: { backgroundColor: "brown" } }}></Stack.Screen>
+        <Stack.Screen name="Sign-up" component={SignupScreen} options={{ headerStyle: { backgroundColor: "lightblue" }}}></Stack.Screen>
+        <Stack.Screen name="Profile" component={ProfileScreen} options={{ headerStyle: { backgroundColor: "pink" }}}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>  
+  )
+}
+export default App;
+
+
+function LoginScreen({navigation}){
+ 
+  return(
+<View style={{backgroundColor:'pink',alignItems:'center',justifyContent:'center'}}>
+      <View>
+        <Text>LoginScreen</Text>
       </View>
-      <View style={styles.wrapper}>
-      <View style={styles.wrapper2}>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-        </View>
-        <View style={styles.wrapper2}>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-        </View>
-        <View style={styles.wrapper2}>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-        </View>
-        <View style={styles.wrapper2}>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-        </View>
-        <View style={styles.wrapper2}>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-        </View>
-        <View style={styles.wrapper2}>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-        </View>
-        <View style={styles.sqBox}></View>
-        <View style={styles.wrapper2}>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-        </View>
-        <View style={styles.wrapper2}>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-        </View>
-        <View style={styles.wrapper2}>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-        </View>
-        <View style={styles.wrapper2}>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-        </View>
-        <View style={styles.wrapper2}>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-        </View>
-        <View style={styles.wrapper2}>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-        </View>
-      </View>
-      <View style={styles.wrapper}>
-        <View style={styles.Box3}>
-          <View style={styles.box3}>
-            <View style={styles.row}>
-              <View style={styles.circle}></View>
-              <View style={styles.circle}></View>
-            </View>
-            <View style={styles.row}>
-              <View style={styles.circle}></View>
-              <View style={styles.circle}></View>
-            </View>
-          </View>
-        </View>
-        <View style={styles.wrapper2}>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-        </View>
-        <View style={styles.wrapper2}>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-        </View>
-        <View style={styles.wrapper2}>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-          <View style={styles.sbox}></View>
-        </View>
-        <View style={styles.Box4}>
-          <View style={styles.box4}>
-            <View style={styles.row}>
-              <View style={styles.circle}></View>
-              <View style={styles.circle}></View>
-            </View>
-            <View style={styles.row}>
-              <View style={styles.circle}></View>
-              <View style={styles.circle}></View>
-            </View>
-          </View>
-        </View>
-      </View>
+      <View style={{ flex:1,backgroundColor: 'grey',height:500,alignItems:'center',justifyContent:'space-around'}}>
+      <View style = {{height:100}}></View>
+          <Image style={{backgroundColor:'transparent',height:100,width:100}}source={{uri:'https://cms-assets.tutsplus.com/cdn-cgi/image/width=400/uploads/users/798/posts/27376/preview_image/firebase@2x.png'}}/>
+          <View style = {{height:80}}></View>
+          <TextInput style = {{width:250,height:30,borderWidth:2}} placeholder='Username'></TextInput>
+          <View style = {{height:10}}></View>
+          <TextInput style = {{width:250,height:30,borderWidth:2}} placeholder='Password' secureTextEntry='true'></TextInput>
+          <View style = {{height:10}}></View>
+          <Pressable style = {{backgroundColor:'blue',width:250,height:20,fontWeight:'bold',alignItems:'center',justifyContent:'center'}} onPress={() => ('Pressed')}>
+            <Text>LOGIN</Text>
+          </Pressable>
+            <Text>You don't have an account? Create one</Text>
+          
+           </View>
+            <Button title="Go to SignupScreen" onPress={()=>navigation.navigate("Sign-up")}/>
+     
     </View>
   );
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "black",
-    justifyContent: "center",
-  },
-  wrapper: {
-    flexDirection: "row",
-    justifyContent: "center",
-    backgroundColor: "black",
-  },
-  wrapper2: {
-    flexDirection: "column",
-    justifyContent: "center",
-  },
-  Box1: {
-    backgroundColor: "red",
-    height: 150,
-    width: 150,
-  },
-  box1: {
-    backgroundColor: "maroon",
-    height: 100,
-    width: 100,
-    margin: 25,
-  },
-  Box2: {
-    backgroundColor: "gold",
-    height: 150,
-    width: 150,
-  },
-  box2: {
-    backgroundColor: "goldenrod",
-    height: 100,
-    width: 100,
-    margin: 25,
-  },
-  Box3: {
-    backgroundColor: "green",
-    height: 150,
-    width: 150,
-  },
-  box3: {
-    backgroundColor: "darkgreen",
-    height: 100,
-    width: 100,
-    margin: 25,
-  },
-  Box4: {
-    backgroundColor: "blue",
-    height: 150,
-    width: 150,
-  },
-  box4: {
-    backgroundColor: "darkblue",
-    height: 100,
-    width: 100,
-    margin: 25,
-  },
-  circle: {
-    backgroundColor: "white",
-    height: 20,
-    width: 20,
-    borderRadius: 20,
-    marginLeft: 10,
-    marginTop: 18,
-    marginRight: 10,
-    justifyContent: "center",
-  },
-  row: {
-    justifyContent: "center",
-    flexDirection: "row",
-  },
-  sbox: {
-    justifyContent: "center",
-    backgroundColor: "white",
-    height: 25,
-    width: 25,
-    borderRadius:6
-  },
-  sqBox:{
-    width:75,
-    height:75,
-    backgroundColor:'brown',
-    borderRadius:10
-  }
-});
+
+
+
+function SignupScreen({navigation}){
+  
+  return(
+<View style={{backgroundColor:'brown',alignItems:'center',justifyContent:'center'}}>
+      <View>
+        <Text>SignupScreen</Text>
+      </View>
+      <View style={{ flex:1,backgroundColor: 'grey',height:500,alignItems:'center',justifyContent:'space-around'}}>
+      <View style = {{height:100}}></View>
+      <Image style={{backgroundColor:'transparent',height:100,width:100}}source={{uri:"https://www.w3schools.com/howto/img_avatar2.png"}}/>
+                <View style = {{height:30}}></View>
+         <TextInput style = {{width:250,height:30,borderWidth:2}} placeholder='Username'></TextInput>
+          <View style = {{height:10}}></View>
+          <TextInput style = {{width:250,height:30,borderWidth:2}} placeholder='Email' ></TextInput>
+          <View style = {{height:10}}></View>
+          <TextInput style = {{width:250,height:30,borderWidth:2}} placeholder='Contact-No'></TextInput>
+          <View style = {{height:10}}></View>
+          <TextInput style = {{width:250,height:30,borderWidth:2}} placeholder='Password' secureTextEntry='true'></TextInput>
+          <View style = {{height:10}}></View>
+          <Pressable style = {{backgroundColor:'blue',width:250,height:20,fontWeight:'bold',alignItems:'center',justifyContent:'center'}}  onPress={() => ('Pressed')}>
+            <Text>SIGNUP</Text>
+          </Pressable>
+           </View>
+          <Button title="Go to ProfileScreen" onPress={()=>navigation.navigate("Profile")}/>
+    </View>
+  )
+
+}
+function ProfileScreen({navigation}){
+  
+  return(
+      <View style={{backgroundColor:'purple',alignItems:'center',justifyContent:'center'}}>
+            <View>
+              <Text>ProfileScreen</Text>
+            </View>
+            <View style={{ flex:1,backgroundColor: 'grey',height:500,alignItems:'center',justifyContent:'space-around'}}>
+            <View style = {{height:100}}></View>
+            <Text>Please complete your profile</Text>
+            
+              <Image style={{backgroundColor:'transparent',height:100,width:100}}source={{uri:"https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png"}}/>
+                <View style = {{height:30}}></View>
+                <TextInput style = {{width:250,height:30,borderWidth:2}} placeholder='Username'></TextInput>
+                <View style = {{height:10}}></View>
+                <TextInput style = {{width:250,height:30,borderWidth:2}} placeholder='Address'></TextInput>
+                <View style = {{height:10}}></View>
+                <TextInput style = {{width:250,height:30,borderWidth:2}} placeholder='City'></TextInput>
+                <View style = {{height:10}}></View>
+                <TextInput style = {{width:250,height:30,borderWidth:2}} placeholder='Country'></TextInput>
+                <View style = {{height:10}}></View>
+                </View>
+                  <Button title="Go to LoginScreen" onPress={()=>navigation.navigate("Login")}/>
+           
+          </View>
+        );
+      }
 
 
