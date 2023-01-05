76d896d5c5f54a570af4d190b4ee726966badf4b
iqrabakhtawar <103189596+iqrabakhtawar@users.noreply.github.com>
2023-01-05 23:14:36 +0500

    Add files via upload

--- Theory Assignment/App.js
+++ Theory Assignment/App.js
@@ -1,70 +1,105 @@
-import React, { useEffect, useState } from 'react';
- import { ActivityIndicator, FlatList, Text, View , StyleSheet, Button } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import {View,Text,Button,Pressable,TextInput,Image} from 'react-native';
 
-export default function Althan({ navigation }) {
-    const [prayer, setprayer] = useState('');
 
-    const getPrayerTime = async () => {
-        const ResposeType = {
-          method: 'Get',
-          latitude: '51.75865125',
-           longitude: '-1.25387785',
-             month: '8',
-             year:'2022',
-
-        }
-        try {
-            const response = await fetch('http://api.aladhan.com/v1/calendar?latitude=32.585411&longitude=71.54361700000004&method=2&month=12&year=2022.json', getPrayerTime);
-            const json = await response.json();
-              
-            setprayer(json.data)
-        } catch (error) {
-            console.error(error);
-        }
-    }
-
-    useEffect(() => {
-        getPrayerTime();
-    }, []);
-console.log('', prayer)
-    return (
-        <View style={styles.container}>
-        
-            <FlatList
-                data={prayer}
-               
-                renderItem={({ item }) => (
-                    <View>
-                    <Text style={styles.text}> {item.timings.Asr}</Text>
-                    <Text style={styles.text}> {item.date.readable}</Text>
-                    
-                   </View>
-                    
-                    
-                
-                )}
-            />
-
-        </View>
-    );
+const Stack=createNativeStackNavigator();
+const App =() =>
+{
+  return(
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} options={{ headerStyle: { backgroundColor: "brown" } }}></Stack.Screen>
+        <Stack.Screen name="Sign-up" component={SignupScreen} options={{ headerStyle: { backgroundColor: "lightblue" }}}></Stack.Screen>
+        <Stack.Screen name="Profile" component={ProfileScreen} options={{ headerStyle: { backgroundColor: "pink" }}}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>  
+  )
 }
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "green",
-        alignItems: "center",
-        justifyContent: "center",
-      },
-      text:{
-        borderColor:"purple",
-        borderWidth:2,
-        borderRadius:10,
-      padding:1,
-        justifyContent:"center",
-        fontSize:30,
-        color:"black"
-    
-        
-      }
-})
+export default App;
 
+
+function LoginScreen({navigation}){
+ 
+  return(
+<View style={{backgroundColor:'pink',alignItems:'center',justifyContent:'center'}}>
+      <View>
+        <Text>LoginScreen</Text>
+      </View>
+      <View style={{ flex:1,backgroundColor: 'grey',height:500,alignItems:'center',justifyContent:'space-around'}}>
+      <View style = {{height:100}}></View>
+          <Image style={{backgroundColor:'transparent',height:100,width:100}}source={{uri:'https://cms-assets.tutsplus.com/cdn-cgi/image/width=400/uploads/users/798/posts/27376/preview_image/firebase@2x.png'}}/>
+          <View style = {{height:80}}></View>
+          <TextInput style = {{width:250,height:30,borderWidth:2}} placeholder='Username'></TextInput>
+          <View style = {{height:10}}></View>
+          <TextInput style = {{width:250,height:30,borderWidth:2}} placeholder='Password' secureTextEntry='true'></TextInput>
+          <View style = {{height:10}}></View>
+          <Pressable style = {{backgroundColor:'blue',width:250,height:20,fontWeight:'bold',alignItems:'center',justifyContent:'center'}} onPress={() => ('Pressed')}>
+            <Text>LOGIN</Text>
+          </Pressable>
+            <Text>You don't have an account? Create one</Text>
+          
+           </View>
+            <Button title="Go to SignupScreen" onPress={()=>navigation.navigate("Sign-up")}/>
+     
+    </View>
+  );
+}
+
+
+
+function SignupScreen({navigation}){
+  
+  return(
+<View style={{backgroundColor:'brown',alignItems:'center',justifyContent:'center'}}>
+      <View>
+        <Text>SignupScreen</Text>
+      </View>
+      <View style={{ flex:1,backgroundColor: 'grey',height:500,alignItems:'center',justifyContent:'space-around'}}>
+      <View style = {{height:100}}></View>
+      <Image style={{backgroundColor:'transparent',height:100,width:100}}source={{uri:"https://www.w3schools.com/howto/img_avatar2.png"}}/>
+                <View style = {{height:30}}></View>
+         <TextInput style = {{width:250,height:30,borderWidth:2}} placeholder='Username'></TextInput>
+          <View style = {{height:10}}></View>
+          <TextInput style = {{width:250,height:30,borderWidth:2}} placeholder='Email' ></TextInput>
+          <View style = {{height:10}}></View>
+          <TextInput style = {{width:250,height:30,borderWidth:2}} placeholder='Contact-No'></TextInput>
+          <View style = {{height:10}}></View>
+          <TextInput style = {{width:250,height:30,borderWidth:2}} placeholder='Password' secureTextEntry='true'></TextInput>
+          <View style = {{height:10}}></View>
+          <Pressable style = {{backgroundColor:'blue',width:250,height:20,fontWeight:'bold',alignItems:'center',justifyContent:'center'}}  onPress={() => ('Pressed')}>
+            <Text>SIGNUP</Text>
+          </Pressable>
+           </View>
+          <Button title="Go to ProfileScreen" onPress={()=>navigation.navigate("Profile")}/>
+    </View>
+  )
+
+}
+function ProfileScreen({navigation}){
+  
+  return(
+      <View style={{backgroundColor:'purple',alignItems:'center',justifyContent:'center'}}>
+            <View>
+              <Text>ProfileScreen</Text>
+            </View>
+            <View style={{ flex:1,backgroundColor: 'grey',height:500,alignItems:'center',justifyContent:'space-around'}}>
+            <View style = {{height:100}}></View>
+            <Text>Please complete your profile</Text>
+            
+              <Image style={{backgroundColor:'transparent',height:100,width:100}}source={{uri:"https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png"}}/>
+                <View style = {{height:30}}></View>
+                <TextInput style = {{width:250,height:30,borderWidth:2}} placeholder='Username'></TextInput>
+                <View style = {{height:10}}></View>
+                <TextInput style = {{width:250,height:30,borderWidth:2}} placeholder='Address'></TextInput>
+                <View style = {{height:10}}></View>
+                <TextInput style = {{width:250,height:30,borderWidth:2}} placeholder='City'></TextInput>
+                <View style = {{height:10}}></View>
+                <TextInput style = {{width:250,height:30,borderWidth:2}} placeholder='Country'></TextInput>
+                <View style = {{height:10}}></View>
+                </View>
+                  <Button title="Go to LoginScreen" onPress={()=>navigation.navigate("Login")}/>
+           
+          </View>
+        );
+      }
